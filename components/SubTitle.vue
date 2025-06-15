1f234ac700fd6bbe70061f516a145d2a997c2a6d
krabbe16 <57534374+krabbe16@users.noreply.github.com>
2025-06-15 16:34:11 +0900

    Update SubTitle.vue

--- components/SubTitle.vue
+++ components/SubTitle.vue
@@ -2,7 +2,7 @@
   <p class="lead text-muted">
     Source code is here !
     <a
-      href="https://github.com/krabben16/test-nuxt-intersection-observer"
+      href="https://github.com/krabbe16/test-nuxt-intersection-observer"
       target="blank"
       rel="noopener noreferrer"
     >
